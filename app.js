6756f76f6b5edbd68abf401e3a0295a3f40477ca
zhaitao <370985116@qq.com>
2018-04-25 20:32:08 +0800

    GET request

--- app.js
+++ app.js
@@ -1,34 +1,24 @@
 const Koa = require('koa')
-const fs = require('fs')
-const Router = require('koa-router')
-
 const app = new Koa()
 
-let home = new Router()
+app.use(async(ctx) => {
+	let url = ctx.url
+	let request = ctx.request
+	let req_query = request.query
+	let req_querystring = request.querystring
 
-home.get('/',async(ctx)=>{
-	let html = `
-		<ul>
-			<li><a href="/page/helloworld">/page/helloworld</a></li>
-      		<li><a href="/page/404">/page/404</a></li>
-		</ul>
-	`
-	ctx.body = html
+	let ctx_query = ctx.query
+	let ctx_querystring = ctx.querystring
+
+	ctx.body = {
+		url,
+		req_query,
+		req_querystring,
+		ctx_query,
+		ctx_querystring
+	}
 })
 
-let page = new Router()
-
-page.get('/404',async(ctx) => {
-	ctx.body = '404 page!'
-}).get('/helloworld',async(ctx) => {
-	ctx.body = 'helloworld page!'
-})
-
-let router = new Router()
-router.use('/',home.routes(),home.allowedMethods())
-router.use('/page',page.routes(),page.allowedMethods())
-
-app.use(router.routes()).use(router.allowedMethods())
 
 app.listen(3000)
 console.log('staring at port 3000')
