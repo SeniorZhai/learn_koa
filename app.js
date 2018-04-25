835516f4e678624bb8c87f02bec38c4c6782f8a9
zhaitao <370985116@qq.com>
2018-04-25 20:26:57 +0800

    koa-router

--- app.js
+++ app.js
@@ -1,50 +1,34 @@
 const Koa = require('koa')
 const fs = require('fs')
+const Router = require('koa-router')
 
 const app = new Koa()
 
+let home = new Router()
 
-function render( page ) {
-  return new Promise(( resolve, reject ) => {
-    let viewUrl = `./view/${page}`
-    fs.readFile(viewUrl, "binary", ( err, data ) => {
-      if ( err ) {
-        reject( err )
-      } else {
-        resolve( data )
-      }
-    })
-  })
-}
+home.get('/',async(ctx)=>{
+	let html = `
+		<ul>
+			<li><a href="/page/helloworld">/page/helloworld</a></li>
+      		<li><a href="/page/404">/page/404</a></li>
+		</ul>
+	`
+	ctx.body = html
+})
 
-async function route(url) {
-	let view = '404.html'
-	switch(url) {
-		case '/':
-			view = 'index.html'
-			break
-		case '/index':
-			view = 'index.html'
-			break
-		case '/todo':
-			view = 'todo.html'
-			break
-		case '/404':
-			view = '404.html'
-			break
-		default:
-			break	
-	}
-	let html = await render(view)
-	return html
-}
+let page = new Router()
 
-app.use( async( ctx ) => {
-		let url = ctx.request.url
-		console.log(url)
-		let html = await route(url)
-		ctx.body = html
-	})
+page.get('/404',async(ctx) => {
+	ctx.body = '404 page!'
+}).get('/helloworld',async(ctx) => {
+	ctx.body = 'helloworld page!'
+})
+
+let router = new Router()
+router.use('/',home.routes(),home.allowedMethods())
+router.use('/page',page.routes(),page.allowedMethods())
+
+app.use(router.routes()).use(router.allowedMethods())
 
 app.listen(3000)
 console.log('staring at port 3000')
