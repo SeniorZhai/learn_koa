a95c98338bad225e9288f579251becca6b66fbfb
zhaitao <370985116@qq.com>
2018-04-25 20:34:10 +0800

    koa Post

--- app.js
+++ app.js
@@ -1,24 +1,61 @@
 const Koa = require('koa')
 const app = new Koa()
 
-app.use(async(ctx) => {
-	let url = ctx.url
-	let request = ctx.request
-	let req_query = request.query
-	let req_querystring = request.querystring
+app.use( async ( ctx ) => {
 
-	let ctx_query = ctx.query
-	let ctx_querystring = ctx.querystring
-
-	ctx.body = {
-		url,
-		req_query,
-		req_querystring,
-		ctx_query,
-		ctx_querystring
-	}
+  if ( ctx.url === '/' && ctx.method === 'GET' ) {
+    // 当GET请求时候返回表单页面
+    let html = `
+      <h1>koa2 request post demo</h1>
+      <form method="POST" action="/">
+        <p>userName</p>
+        <input name="userName" /><br/>
+        <p>nickName</p>
+        <input name="nickName" /><br/>
+        <p>email</p>
+        <input name="email" /><br/>
+        <button type="submit">submit</button>
+      </form>
+    `
+    ctx.body = html
+  } else if ( ctx.url === '/' && ctx.method === 'POST' ) {
+    // 当POST请求的时候，解析POST表单里的数据，并显示出来
+    let postData = await parsePostData( ctx )
+    ctx.body = postData
+  } else {
+    // 其他请求显示404
+    ctx.body = '<h1>404！！！ o(╯□╰)o</h1>'
+  }
 })
 
+function parsePostData( ctx ) {
+  return new Promise((resolve, reject) => {
+    try {
+      let postdata = "";
+      ctx.req.addListener('data', (data) => {
+        postdata += data
+      })
+      ctx.req.addListener("end",function(){
+        let parseData = parseQueryStr( postdata )
+        resolve( parseData )
+      })
+    } catch ( err ) {
+      reject(err)
+    }
+  })
+}
+
+// 将POST请求参数字符串解析成JSON
+function parseQueryStr( queryStr ) {
+  let queryData = {}
+  let queryStrList = queryStr.split('&')
+  console.log( queryStrList )
+  for (  let [ index, queryStr ] of queryStrList.entries()  ) {
+    let itemList = queryStr.split('=')
+    queryData[ itemList[0] ] = decodeURIComponent(itemList[1])
+  }
+  return queryData
+}
 
 app.listen(3000)
 console.log('staring at port 3000')
