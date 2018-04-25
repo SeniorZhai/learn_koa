3de19af746d84a4d81d6fbeb8f70fd39d361e07e
zhaitao <370985116@qq.com>
2018-04-25 20:15:41 +0800

    koa router

--- app.js
+++ app.js
@@ -1,12 +1,49 @@
 const Koa = require('koa')
-
-const loggerAsync = require('./middleware/logger-async')
+const fs = require('fs')
 
 const app = new Koa()
 
-app.use(loggerAsync())
-app.use(( ctx ) => {
-		ctx.body = 'hello koa2'
+
+function render( page ) {
+  return new Promise(( resolve, reject ) => {
+    let viewUrl = `./view/${page}`
+    fs.readFile(viewUrl, "binary", ( err, data ) => {
+      if ( err ) {
+        reject( err )
+      } else {
+        resolve( data )
+      }
+    })
+  })
+}
+
+async function route(url) {
+	let view = '404.html'
+	switch(url) {
+		case '/':
+			view = 'index.html'
+			break
+		case '/index':
+			view = 'index.html'
+			break
+		case '/todo':
+			view = 'todo.html'
+			break
+		case '/404':
+			view = '404.html'
+			break
+		default:
+			break	
+	}
+	let html = await render(view)
+	return html
+}
+
+app.use( async( ctx ) => {
+		let url = ctx.request.url
+		console.log(url)
+		let html = await route(url)
+		ctx.body = html
 	})
 
 app.listen(3000)
