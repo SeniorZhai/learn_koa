af650b7d57769014406c145ac932a6e0b6f70560
zhaitao <370985116@qq.com>
2018-04-25 20:44:11 +0800

    post

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const Koa = require('koa')
 const app = new Koa()
-
+const bodyParser = require('koa-bodyparser')
+// 中间件解析数据
+app.use(bodyParser())
 app.use( async ( ctx ) => {
 
   if ( ctx.url === '/' && ctx.method === 'GET' ) {
@@ -20,42 +22,13 @@ app.use( async ( ctx ) => {
     ctx.body = html
   } else if ( ctx.url === '/' && ctx.method === 'POST' ) {
     // 当POST请求的时候，解析POST表单里的数据，并显示出来
-    let postData = await parsePostData( ctx )
+    let postData = ctx.request.body
     ctx.body = postData
   } else {
     // 其他请求显示404
     ctx.body = '<h1>404！！！ o(╯□╰)o</h1>'
   }
 })
-
-function parsePostData( ctx ) {
-  return new Promise((resolve, reject) => {
-    try {
-      let postdata = "";
-      ctx.req.addListener('data', (data) => {
-        postdata += data
-      })
-      ctx.req.addListener("end",function(){
-        let parseData = parseQueryStr( postdata )
-        resolve( parseData )
-      })
-    } catch ( err ) {
-      reject(err)
-    }
-  })
-}
-
-// 将POST请求参数字符串解析成JSON
-function parseQueryStr( queryStr ) {
-  let queryData = {}
-  let queryStrList = queryStr.split('&')
-  console.log( queryStrList )
-  for (  let [ index, queryStr ] of queryStrList.entries()  ) {
-    let itemList = queryStr.split('=')
-    queryData[ itemList[0] ] = decodeURIComponent(itemList[1])
-  }
-  return queryData
-}
 
 app.listen(3000)
 console.log('staring at port 3000')
