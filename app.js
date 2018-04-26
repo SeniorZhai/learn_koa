4bfa122fdb24e14ae96cf4b498bec3ec6b102811
zhaitao <370985116@qq.com>
2018-04-26 18:04:02 +0800

    cookies

--- app.js
+++ app.js
@@ -1,33 +1,25 @@
 const Koa = require('koa')
-const path = require('path')
-const content = require('./util/content')
-const mimes = require('./util/mimes')
 
 const app = new Koa()
 
-const staticPath = './static'
-
-// 资源类型
-function parseMime(url) {
-  let extName = path.extname( url )
-  extName = extName ?  extName.slice(1) : 'unknown'
-  return  mimes[ extName ]
-}
 
 app.use( async ( ctx ) => {
-  let fullStaticPath = path.join(__dirname,staticPath)
-  let _content = await content(ctx,fullStaticPath)
-  let _mime = parseMime(ctx.url)
-  if (_mime) {
-    ctx.type = _mime
-  }
-
-  if (_mime && _mime.indexOf('image/') >= 0) {
-    ctx.res.writeHead(200)
-    ctx.res.write(_content,'binary')
-    ctx.res.end()
-  }else{
-    ctx.body = _content
+  if (ctx.url === '/index') {
+    ctx.cookies.set(
+      'cid',
+      'hello world',
+      {
+        domain:'localhost', // 所在域名
+        path:'/index', // 所在路径
+        maxAge: 10 * 60 *1000, // 有效时长
+        expires: new Date('2018-05-01'), // 失效时间
+        httpOnly:false, // 只用于http请求
+        overwrite:false // 不允许重写
+      }
+    )
+    ctx.body = 'cookie is ok'
+  }else {
+    ctx.body = 'hello world'
   }
 })
 
