63e1012009e378f4bd96dcb30b13e830ea3f6be2
zhaitao <370985116@qq.com>
2018-04-26 18:09:28 +0800

    clear

--- app.js
+++ app.js
@@ -4,23 +4,7 @@ const app = new Koa()
 
 
 app.use( async ( ctx ) => {
-  if (ctx.url === '/index') {
-    ctx.cookies.set(
-      'cid',
-      'hello world',
-      {
-        domain:'localhost', // 所在域名
-        path:'/index', // 所在路径
-        maxAge: 10 * 60 *1000, // 有效时长
-        expires: new Date('2018-05-01'), // 失效时间
-        httpOnly:false, // 只用于http请求
-        overwrite:false // 不允许重写
-      }
-    )
-    ctx.body = 'cookie is ok'
-  }else {
-    ctx.body = 'hello world'
-  }
+  ctx.body="hello"
 })
 
 app.listen(3000)
